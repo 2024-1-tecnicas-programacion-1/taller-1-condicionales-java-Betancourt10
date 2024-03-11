a9e59c6fc84398483d6e973fd7452ede941f6d68
Betancourt10 <Felibetan2005@gmail.com>
2024-03-11 06:10:09 -0500

    Test IMC.

--- src/test/java/test/IMCTest.java
+++ src/test/java/test/IMCTest.java
@@ -15,10 +15,37 @@ import static org.junit.jupiter.api.Assertions.*;
 public class IMCTest {
     @Test
     public void testBajo() {
-        String valorEsperado = "bajo";
+        String valorEsperado = "Bajo";
         String valorActual = IMC.evaluar(50, 1.8,   20);
         assertEquals(valorEsperado, valorActual);
     }
     
-    // TODO: Agrega tus otros casos de prueba aquí
+    @Test
+    public void testMedio() {
+        String valorEsperado = "Medio";
+        String valorActual = IMC.evaluar(76, 1.69,   34);
+        assertEquals(valorEsperado, valorActual);
+    } 
+    
+    @Test
+    public void testMedio2() {
+        String valorEsperado = "Medio";
+        String valorActual = IMC.evaluar(44, 1.7,   52);
+        assertEquals(valorEsperado, valorActual);
+    } 
+    
+    @Test
+    public void testAlto() {
+        String valorEsperado = "Alto";
+        String valorActual = IMC.evaluar(70, 1.65,   73);
+        assertEquals(valorEsperado, valorActual);
+    } 
+    
+    @Test
+    public void testError() {
+        String valorEsperado = "Error";
+        String valorActual = IMC.evaluar(-2, 1.8,   18);
+        assertEquals(valorEsperado, valorActual);
+    }
+    
 }
