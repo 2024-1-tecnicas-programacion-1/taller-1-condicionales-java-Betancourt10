375fa45363d445dde406bc55f1a8bbeb5c92b911
Betancourt10 <Felibetan2005@gmail.com>
2024-03-11 06:09:51 -0500

    IMC.

--- src/main/java/ejercicios/IMC.java
+++ src/main/java/ejercicios/IMC.java
@@ -1,7 +1,4 @@
-
 package ejercicios;
-
-import static ejercicios.SetDeTenis.evaluar;
 import java.util.Scanner;
 
 /**
@@ -9,14 +6,47 @@ import java.util.Scanner;
  * @author danielsanchez
  */
 public class IMC {
-    public static String evaluar(int peso, double estatura, int edad) {
-        // TODO: Coloca aquí el código del ejercicio 8: Índice de masa corporal
-        return "";
-    }
+    public static String evaluar(int peso, double estatura, int edad) { 
+        double total = peso / Math.pow(estatura, 2); 
+        if (edad < 45 && edad > 0) { 
+            if (total < 22 && total > 0) 
+                { 
+                    return "Bajo"; 
+                } 
+            else if (total >= 22 && total > 0) 
+                { 
+                    return "Medio"; 
+                } 
+            else if (total < 0) 
+                { 
+                    return "Error"; 
+                }  
+        } 
+        else if (edad >= 45) { 
+            if (total < 22) 
+                { 
+                    return "Medio"; 
+                } 
+            else if (total >= 22) 
+                { 
+                    return "Alto"; 
+                } 
+            else if (total < 0) 
+                { 
+                    return "Error";
+                } 
+        }
+            else 
+                { 
+                    return "Error";
+                } 
+        return null;
+        }
+
     
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-            System.out.print("Peso:");
+        System.out.print("Peso:");
         int peso = lector.nextInt();
         System.out.print("Estatura:");
         double estatura = lector.nextDouble();
