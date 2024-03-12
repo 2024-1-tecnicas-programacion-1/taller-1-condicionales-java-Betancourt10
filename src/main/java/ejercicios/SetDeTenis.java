e5ddccfbdf3b5939cce16339a7a5fdf88b7c9ffc
Betancourt10 <Felibetan2005@gmail.com>
2024-03-12 00:47:13 -0500

    SetDeTenis.

--- src/main/java/ejercicios/SetDeTenis.java
+++ src/main/java/ejercicios/SetDeTenis.java
@@ -7,9 +7,39 @@ import java.util.Scanner;
  * @author danielsanchez
  */
 public class SetDeTenis {
-    public static String evaluar(int numVictoriasA, int numVictoriasB) {
-        // TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-        return "";
+    public static String evaluar(int numVictoriasA, int numVictoriasB) { 
+        if (numVictoriasA < 0 || numVictoriasB < 0) 
+            { 
+                return "Error.";
+            }
+        else if (numVictoriasA > 7 || numVictoriasB > 7) 
+            { 
+                return "Inválido.";
+            } 
+        else if (numVictoriasA >= 6 && (numVictoriasA - numVictoriasB) >= 2) 
+            { 
+                return "Ganó A."; 
+            } 
+        else if (numVictoriasB >= 6 && (numVictoriasB - numVictoriasA) >= 2) 
+            { 
+                return "Ganó B."; 
+            } 
+        else if (numVictoriasA <= 6 && numVictoriasB <= 6) 
+            { 
+                return "Aún no termina."; 
+            } 
+        else if (numVictoriasA == 7 && numVictoriasB == 6)
+            { 
+                return "Ganó A.";
+            } 
+        else if (numVictoriasA == 6 && numVictoriasB == 7)
+            { 
+                return "Ganó B.";
+            }
+        else 
+            { 
+                return "Inválido.";
+            }
     }
     
     public static void main(String[] args) {
