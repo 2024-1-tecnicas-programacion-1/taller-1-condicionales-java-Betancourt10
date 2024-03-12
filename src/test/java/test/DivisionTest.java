d71e3edb8a5c441c8f6d912ea04fbc8c87918a87
Betancourt10 <Felibetan2005@gmail.com>
2024-03-12 00:45:32 -0500

    Corrección Test División.

--- src/test/java/test/DivisionTest.java
+++ src/test/java/test/DivisionTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import ejercicios.Division;
-import ejercicios.SetDeTenis;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,38 +15,45 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DivisionTest {
     @Test
     public void testDivisionNoExacta() {
-        String valorEsperado = "La división no es exacta. \n"
+        String respuesta = "La división no es exacta. \n"
                 + "Cociente: 2\n"
                 + "Residuo: 4";
         String valorActual = Division.evaluar(14, 5);
-        assertEquals(valorEsperado, valorActual);
+        assertEquals(respuesta, valorActual);
     } 
     
     @Test
     public void testDivisionExacta() {
-        String valorEsperado = "La división es exacta. \n"
+        String respuesta = "La división es exacta. \n"
                 + "Cociente: 10\n"
                 + "Residuo: 0";
         String valorActual = Division.evaluar(100, 10);
-        assertEquals(valorEsperado, valorActual);
+        assertEquals(respuesta, valorActual);
     } 
     
     @Test
     public void testDivisionNoExactaDos() {
-        String valorEsperado = "La división no es exacta. \n"
+        String respuesta = "La división no es exacta. \n"
                 + "Cociente: 6\n"
                 + "Residuo: 2";
         String valorActual = Division.evaluar(32, 5);
-        assertEquals(valorEsperado, valorActual);
+        assertEquals(respuesta, valorActual);
     } 
     
     @Test
     public void testDivisionExactaDos() {
-        String valorEsperado = "La división es exacta. \n"
+        String respuesta = "La división es exacta. \n"
                 + "Cociente: 7\n"
                 + "Residuo: 0";
         String valorActual = Division.evaluar(56, 8);
-        assertEquals(valorEsperado, valorActual);
+        assertEquals(respuesta, valorActual);
     } 
+    
+    @Test
+    public void testDivisionNoValida() {
+        String respuesta = "La división no es válida.";
+        String valorActual = Division.evaluar(2, 0);
+        assertEquals(respuesta, valorActual);
+    }
     
 }
