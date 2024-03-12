a4459829eeb500d3553f4550175cc05a50a0155d
Betancourt10 <Felibetan2005@gmail.com>
2024-03-12 00:47:38 -0500

    Test SetDeTenis.

--- src/test/java/test/SetDeTenisTest.java
+++ src/test/java/test/SetDeTenisTest.java
@@ -15,10 +15,51 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SetDeTenisTest {
     @Test
     public void testAunNoTermina() {
-        String valorEsperado = "Aún no termina";
+        String valorEsperado = "Aún no termina.";
         String valorActual = SetDeTenis.evaluar(4, 5);
         assertEquals(valorEsperado, valorActual);
     }
     
-    // TODO: Agrega tus otros casos de prueba aquí
+    @Test
+    public void testGanoA() {
+        String valorEsperado = "Ganó A.";
+        String valorActual = SetDeTenis.evaluar(7, 5);
+        assertEquals(valorEsperado, valorActual);
+    } 
+    
+    @Test
+    public void testGanoB() {
+        String valorEsperado = "Ganó B.";
+        String valorActual = SetDeTenis.evaluar(3, 6);
+        assertEquals(valorEsperado, valorActual);
+    } 
+    
+    @Test
+    public void testError() {
+        String valorEsperado = "Error.";
+        String valorActual = SetDeTenis.evaluar(-3, 5);
+        assertEquals(valorEsperado, valorActual);
+    } 
+    
+    @Test
+    public void testInvalido() {
+        String valorEsperado = "Inválido.";
+        String valorActual = SetDeTenis.evaluar(8, 6);
+        assertEquals(valorEsperado, valorActual);
+    } 
+    
+    @Test
+    public void testGanoADos() {
+        String valorEsperado = "Ganó A.";
+        String valorActual = SetDeTenis.evaluar(7, 6);
+        assertEquals(valorEsperado, valorActual);
+    } 
+    
+    @Test
+    public void testGanoBDos() {
+        String valorEsperado = "Ganó B.";
+        String valorActual = SetDeTenis.evaluar(6, 7);
+        assertEquals(valorEsperado, valorActual);
+    }
+    
 }
